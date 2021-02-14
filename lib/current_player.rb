972bbb6d9ebdaf65ee5a7be3d7a30f572a739723
Rebecca Sorrell <rebeccassorrell@gmail.com>
2021-02-14 01:27:42 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,23 +1,19 @@
 def turn_count(board)
- counter = 0
-  board.each do |turn|
-
-    if turn == "X" || turn == "O"
+  counter = 0
+  board.each do |spot|
+    if spot == "X" || spot == "O"
       counter += 1
-
     end
   end
-   counter
+  return counter
 end
 
 def current_player(board)
-
- (turn_count(board) % 2) != 1 ? "X" : "O"
-
-    #if (turn_count(board) % 2) != 1
-      #"X"
-
-     #else
-       #"O"
-  #end
-end
+  player = nil
+  if turn_count(board) % 2 == 0
+    player = "X"
+  else 
+    player = "O"
+  end
+  return player
+end 
